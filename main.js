10d59056e8c1d39a49948906dbe96f116b476e78
GonzaloBisio <gonzalobisio5@gmail.com>
2022-06-14 17:16:10 -0300

    Cronometro

--- main.js
+++ main.js
@@ -1,190 +1,155 @@
-const canvas = document.getElementById("canvas");
-canvas.width = 500;
-canvas.height = 350;
+const canvas = document.getElementById("canvas")
+canvas.width = 500
+canvas.height = 350
 
-let context = canvas.getContext("2d");
+let context = canvas.getContext("2d")
 let start_background_color = "white"
-context.fillStyle = start_background_color;
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.fillStyle = start_background_color
+context.fillRect(0, 0, canvas.width, canvas.height)
 
-let draw_color = "black";
-let draw_width = "2";
-let is_drawing = false;
+let draw_color = "black"
+let draw_width = "2"
+let is_drawing = false
 
-let restore_array = [];
-let index = -1;
+let restore_array = []
+let index = -1
 
 function change_color(element) {
-  draw_color = element.style.background;
+  draw_color = element.style.background
 }
 
-canvas.addEventListener("touchstart", start, false);
-canvas.addEventListener("touchmove", draw, false);
-canvas.addEventListener("mousedown", start, false);
-canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("touchstart", start, false)
+canvas.addEventListener("touchmove", draw, false)
+canvas.addEventListener("mousedown", start, false)
+canvas.addEventListener("mousemove", draw, false)
 
-canvas.addEventListener("touchend", parar, false);
-canvas.addEventListener("mouseup", parar, false);
-canvas.addEventListener("mouseout", parar, false);
+canvas.addEventListener("touchend", parar, false)
+canvas.addEventListener("mouseup", parar, false)
+canvas.addEventListener("mouseout", parar, false)
 
-function start(event){
-  is_drawing = true;
-  context.beginPath();
+function start(event) {
+  is_drawing = true
+  context.beginPath()
   context.moveTo(event.clientX - canvas.offsetLeft,
-                 event.clientY - canvas.offsetTop);
-  event.preventDefault();
+    event.clientY - canvas.offsetTop)
+  event.preventDefault()
 }
 
-function draw(event){
+function draw(event) {
   if (is_drawing) {
     context.lineTo(event.clientX - canvas.offsetLeft,
-                   event.clientY - canvas.offsetTop);
-    context.strokeStyle = draw_color;
-    context.lineWidth = draw_width;
-    context.lineCap = "round";
-    context.lineJoin = "round";
-    context.stroke();
+      event.clientY - canvas.offsetTop)
+    context.strokeStyle = draw_color
+    context.lineWidth = draw_width
+    context.lineCap = "round"
+    context.lineJoin = "round"
+    context.stroke()
   }
-  event.preventDefault();
+  event.preventDefault()
 }
 
-function parar(event){
-  if (is_drawing){
-    context.stroke();
-    context.closePath();
-    is_drawing = false;
+function parar(event) {
+  if (is_drawing) {
+    context.stroke()
+    context.closePath()
+    is_drawing = false
   }
-  event.preventDefault();
+  event.preventDefault()
 
   if (event.type != 'mouseout') {
-    restore_array.push(context.getImageData(0,0,canvas.width,canvas.height));
-    index += 1;
+    restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    index += 1
   }
-  
+
 }
 
-function clear_canvas(){
+function clear_canvas() {
   context.fillStyle = start_background_color;
-  context.clearRect(0,0,canvas.width,canvas.height);
-  context.fillRect(0,0,canvas.width,canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.fillRect(0, 0, canvas.width, canvas.height)
 
   restore_array = [];
   index = -1;
 }
 
-function undo_line(){
-  if (index <= 0){
-    clear_canvas();
+function undo_line() {
+  if (index <= 0) {
+    clear_canvas()
   } else {
-    index -= 1;
-    restore_array.pop();
-    context.putImageData(restore_array[index],0,0);
+    index -= 1
+    restore_array.pop()
+    context.putImageData(restore_array[index], 0, 0)
   }
 }
 
 image_array = ["acuatico.png", "bestia.png", "cannonbolt.png", "cuatrobrazos.png", "diamante.png", "fantasmatico.png", "fuego.png", "insectoide.png", "materiagris.png", "ultrat.png", "xlr8.png"];
-const string_array = ["Acuático", "Bestia", "Cannonbolt", "Cuatrobrazos", "Diamante", "Fantasmático", "Fuego", "Insectoide", "Materia Gris", "Ultra T", "XLR-8"]; 
+const string_array = ["Acuático", "Bestia", "Cannonbolt", "Cuatrobrazos", "Diamante", "Fantasmático", "Fuego", "Insectoide", "Materia Gris", "Ultra T", "XLR-8"];
 
 function get_random_image() {
-  random_index = Math.floor(Math.random() * image_array.length);
-  selected_image = image_array[random_index]; 
-  document.getElementById("image_shower").src = "./characters/"+selected_image;
+  random_index = Math.floor(Math.random() * image_array.length)
+  selected_image = image_array[random_index];
+  document.getElementById("image_shower").src = "./characters/" + selected_image
   selected_name = string_array[random_index];
-  document.getElementById("string_shower").innerHTML = selected_name;
+  document.getElementById("string_shower").innerHTML = selected_name
+}
+
+/*
+
+let time = 1
+
+const count = document.getElementById("countdown")
+
+function empezar(){
+  let refreshIntelvalId =setInterval(updateTimer,1000)
 }
 
 
-class Timer {
-  constructor(root) {
-    root.innerHTML = Timer.getHTML();
+function updateTimer() {
+  let seconds = time * 60
 
-    this.el = {
-      minutes: root.querySelector(".timer__part--minutes"),
-      seconds: root.querySelector(".timer__part--seconds"),
-      control: root.querySelector(".timer__btn--control"),
-      reset: root.querySelector(".timer__btn--reset")
-    };
+  seconds = seconds < 10 ? '0' + seconds : seconds
 
-    this.interval = null;
-    this.remainingSeconds = 0;
+  count.innerHTML = `00:${seconds}`
+  time --
 
-    this.el.control.addEventListener("click", () => {
-      if (this.interval === null) {
-        this.start();
-      } else {
-        this.stop();
-      }
-    });
-
-    this.el.reset.addEventListener("click", () => {
-      const inputMinutes = prompt("Enter number of minutes:");
-
-      if (inputMinutes < 60) {
-        this.stop();
-        this.remainingSeconds = inputMinutes * 60;
-        this.updateInterfaceTime();
-      }
-    });
-  }
-
-  updateInterfaceTime() {
-    const minutes = Math.floor(this.remainingSeconds / 60);
-    const seconds = this.remainingSeconds % 60;
-
-    this.el.minutes.textContent = minutes.toString().padStart(2, "0");
-    this.el.seconds.textContent = seconds.toString().padStart(2, "0");
-  }
-
-  updateInterfaceControls() {
-    if (this.interval === null) {
-      this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-      this.el.control.classList.add("timer__btn--start");
-      this.el.control.classList.remove("timer__btn--stop");
-    } else {
-      this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
-      this.el.control.classList.add("timer__btn--stop");
-      this.el.control.classList.remove("timer__btn--start");
-    }
-  }
-
-  start() {
-    if (this.remainingSeconds === 0) return;
-
-    this.interval = setInterval(() => {
-      this.remainingSeconds--;
-      this.updateInterfaceTime();
-
-      if (this.remainingSeconds === 0) {
-        this.stop();
-      }
-    }, 1000);
-
-    this.updateInterfaceControls();
-  }
-
-  stop() {
-    clearInterval(this.interval);
-
-    this.interval = null;
-
-    this.updateInterfaceControls();
-  }
-
-  static getHTML() {
-    return `
-			<span class="timer__part timer__part--minutes">00</span>
-			<span class="timer__part">:</span>
-			<span class="timer__part timer__part--seconds">00</span>
-			<button type="button" class="timer__btn timer__btn--control timer__btn--start">
-				<span class="material-icons">play_arrow</span>
-			</button>
-			<button type="button" class="timer__btn timer__btn--reset">
-				<span class="material-icons">timer</span>
-			</button>
-		`;
-  }
+  if (time < 0) { //stop the setInterval whe time = 0 for avoid negative time
+    clearInterval(refreshIntervalId);
 }
 
-new Timer(
-	document.querySelector(".timer")
-);
+}
+
+*/
+
+document.getElementById('countdown').innerHTML ="0"+2 + ":" + 0+"0";
+
+
+function empezar() {
+  var presentTime = document.getElementById('countdown').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
+  }
+  
+  document.getElementById('countdown').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(empezar, 1000);
+    
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
+const btn = document.getElementById('play');
+
+function ocultar(){
+{
+    btn.style.display = 'none';
+}}
+
